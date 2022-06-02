4535fe6eb0dcf822cf61895fffda205d72893e80
karanRaiBuckleInc <106752070+karanRaiBuckleInc@users.noreply.github.com>
2022-06-02 13:25:30 -0400

    Update repo.rb

--- attributes/repo.rb
+++ attributes/repo.rb
@@ -1,5 +1,5 @@
 default['rundeck_server']['yum']['description'] = 'Rundeck Official Repo'
-default['rundeck_server']['yum']['gpgcheck']    = true
+default['rundeck_server']['yum']['gpgcheck']    = false
 default['rundeck_server']['yum']['enabled']     = false
 default['rundeck_server']['yum']['baseurl']     = 'http://dl.bintray.com/rundeck/rundeck-rpm/'
 default['rundeck_server']['yum']['gpgkey']      = 'http://rundeck.org/keys/BUILD-GPG-KEY-Rundeck.org.key'
